758851fc1fe62bf1c7b4716a707ec1aebf468e45
Nergiz Alıci <nergizalici65@gmail.com>
2025-07-09 15:58:24 +0300

    input tekrar
    focusnode,controller..

---
+++ Flutter/input_tekrar/input_tekrar/lib/main.dart
@@ -0,0 +1,128 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(debugShowCheckedModeBanner: false, home: MyProject());
+  }
+}
+
+class MyProject extends StatelessWidget {
+  const MyProject({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: AppBar(
+          title: Text("Input Tekrar"),
+          backgroundColor: Colors.blue,
+          foregroundColor: Colors.white,
+        ),
+      ),
+      body: TextFieldWidgetKullanimi(),
+    );
+  }
+}
+
+class TextFieldWidgetKullanimi extends StatefulWidget {
+  const TextFieldWidgetKullanimi({super.key});
+
+  @override
+  State<TextFieldWidgetKullanimi> createState() =>
+      _TextFieldWidgetKullanimiState();
+}
+
+class _TextFieldWidgetKullanimiState extends State<TextFieldWidgetKullanimi> {
+  late TextEditingController _emailController;
+  late FocusNode _focusNode;
+  int maxLineCount = 1;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _emailController = TextEditingController();
+    _focusNode = FocusNode();
+    _focusNode.addListener(() {
+      setState(() {
+        maxLineCount = _focusNode.hasFocus ? 3 : 1;
+      });
+    });
+  }
+
+  @override
+  void dispose() {
+    _emailController.dispose();
+    _focusNode.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Padding(
+          padding: const EdgeInsets.all(18.0),
+          child: TextField(
+            focusNode: _focusNode,
+            controller: _emailController,
+            maxLines: maxLineCount,
+            //Açılacak olan klavye türü
+            keyboardType: TextInputType.emailAddress,
+            //Klavyedeki(Android) ana butonunun ne olacağı
+            textInputAction: TextInputAction.done,
+
+            //Seçili gelme olayı
+            autofocus: true,
+            //Satır sayısı
+            //maxLines: maxLineCount,
+            //Girilecek karakter sayısı (TC)
+
+            //İmleç rengi
+            cursorColor: Colors.red,
+            decoration: InputDecoration(
+              //Kayan bilgi yazısı
+              labelText: "Username",
+              //İpucu
+              hintText: "Kullanıcı adınızı giriniz",
+              icon: Icon(Icons.add),
+              //Sol tarafa eklenen icon
+              prefix: Icon(Icons.person),
+              //Sağ taraf iconu
+              suffixIcon: Icon(Icons.cancel),
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(10),
+              ),
+              //Arka plan rengi verme izni
+              filled: true,
+              fillColor: Colors.green.shade300,
+            ),
+            //Klavye ile yapılan her değişikliği algılar
+            onChanged: (String gelenDeger) {},
+            //Klavyedeki done tuşuna basınca çalışır ya da fiel dan çıkınca
+            onSubmitted: (String gelenDeger) {},
+          ),
+        ),
+        TextField(),
+      ],
+    );
+  }
+}
+
+//tek bir birim varsa tekfield kullanılır. çoklu alan varsa textformfiel kullanılır.
+//controllerlar her zaman arka tarafta birikir 
+//global key de bir controllerdır
+//bunlar daima arka planda biriktiği için dispose edimesi gerekir.
+//mesela anasayfada şifre kontrolü sağladım sonra anasayfaya geçtiğinde o şifre controller bilgisi dispose ediliyor
+//önce init sonra dispose
+//dispose kapatıyor
+// TextEditingController içine varsayılan olarak text parametresi alması işimiz kolaylaştırır
+//texfield stateful widgettan türer.
+//nodefocuseda dispose edilir.sayfa değiştikten sonra ortadan kaldırılır.
